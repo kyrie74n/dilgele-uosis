0abcf5a2f8be0f56dc4d96501adf94f3e6c5a1e0
kyrie74n <tlom.hzop@gmail.com>
2017-02-10 23:07:13 +0900

    update datamodels

--- Models/dataModels.js
+++ Models/dataModels.js
@@ -6,9 +6,26 @@ function myViewModel(){
 } ;
 
 function calculateModel(){
-    this.price = ko.observable();
+    this.checkType = ko.observable("HP");
+    this.charge = ko.observable("10");
+    this.tax = ko.observable("10");
+    this.material1name = ko.computed(function(){
+        return this.checkType() == "HP" ? "ヴァレリジョナス×2" : "ヴァルナレーザ×3";
+    }, this);
+    this.material1 = ko.computed(function(){
+        return this.checkType() == "HP" ? 100 : 375;
+    }, this);
+    this.material2name = ko.computed(function(){
+        return this.checkType() == "HP" ? "ディルゲレ" : "ウオシス";
+    }, this);
+    this.material2 = ko.observable(1100);
+    this.material = ko.computed(function(){
+        return this.material1() + 120;
+    }, this);
+    this.personAge = ko.computed(function(){
+        return this.charge();
+    }, this);
     
-    
-}
+};
 
-ko.applyBindings(new myViewModel());
+ko.applyBindings(new calculateModel());
