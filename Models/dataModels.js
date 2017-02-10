bcb52aaac93131e108aa84465cd9d058b0fc701f
kyrie74n <tlom.hzop@gmail.com>
2017-02-11 00:04:02 +0900

    tax logic update

--- Models/dataModels.js
+++ Models/dataModels.js
@@ -2,7 +2,7 @@ function calculateModel(){
     this.checkType = ko.observable("HP");
     this.charge = ko.observable("10");
     this.price = ko.observable(2000);
-    this.tax = ko.observable("10");
+    this.tax = ko.observable("100");
     this.material1name = ko.computed(function(){
         return this.checkType() == "HP" ? "ヴァレリジョナス×2" : "ヴァルナレーザ×3";
     }, this);
@@ -19,13 +19,17 @@ function calculateModel(){
     this.personAge = ko.computed(function(){
         return this.charge();
     }, this);
-
-    this.profit = ko.computed(function(){
-        return this.price() - (this.material() + this.material2())/5 ;
-
+    this.unit = ko.computed(function(){
+        return (Number(this.material()) + Number(this.material2()))/5 ;
     }, this);
-    
-    
+    this.gross = ko.computed(function(){
+        return this.price() - this.unit() ;
+    }, this);
+    this.profit = ko.computed(function(){
+        return this.price()
+            - this.unit()
+            - Number(this.price()) * (Number(this.charge()) + Number(this.tax())) / 1000 ;
+    }, this);
 };
 
 ko.applyBindings(new calculateModel());
